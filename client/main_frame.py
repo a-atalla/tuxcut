daa2a1031619285b8a053a2013738c3c6c8f2680
a.atalla <a.3talla@gmail.com>
2017-09-12 10:47:48 +0200

    add alias feature

--- client/main_frame.py
+++ client/main_frame.py
@@ -2,23 +2,23 @@ import os
 import sys
 import logging
 from pathlib import Path
+import shelve
 import requests
 from threading import Thread
 import wx
 from gui import MainFrame
 import icons
 
-# LOG_DIR = '/var/log/tuxcut'
-LOG_DIR = os.path.join(str(Path.home()), '.tuxcut')
-if not os.path.isdir(LOG_DIR):
-    os.mkdir(LOG_DIR)
-    client_log = Path(os.path.join(LOG_DIR, 'tuxcut.log'))
+APP_DIR = os.path.join(str(Path.home()), '.tuxcut')
+if not os.path.isdir(APP_DIR):
+    os.mkdir(APP_DIR)
+    client_log = Path(os.path.join(APP_DIR, 'tuxcut.log'))
     client_log.touch(exist_ok=True)
     client_log.chmod(0o666)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('tuxcut-client')
-handler = logging.FileHandler(os.path.join(LOG_DIR, 'tuxcut.log'))
+handler = logging.FileHandler(os.path.join(APP_DIR, 'tuxcut.log'))
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
@@ -33,6 +33,7 @@ class MainFrameView(MainFrame):
 
         self.SetIcon(icons.ninja_32.GetIcon())
 
+        self.aliases = shelve.open( os.path.join(APP_DIR, 'aliases.db'))
         # initialize
         self._gw = dict()
         self._my = dict()
@@ -55,7 +56,8 @@ class MainFrameView(MainFrame):
             self.hosts_view.AppendIconTextColumn('', width=30)
             self.hosts_view.AppendTextColumn('IP Address', width=150)
             self.hosts_view.AppendTextColumn('MAC Address', width=150)
-            self.hosts_view.AppendTextColumn('Hostname')
+            self.hosts_view.AppendTextColumn('Hostname', width=200)
+            self.hosts_view.AppendTextColumn('Alias')
 
             self.trigger_thread()
 
@@ -64,13 +66,17 @@ class MainFrameView(MainFrame):
         tbtn_refresh = self.toolbar.AddTool(-1, '', icons.refresh_32.GetBitmap(), shortHelp='Refresh')
         tbtn_cut = self.toolbar.AddTool(-1, '', icons.cut_32.GetBitmap(), shortHelp='Cut')
         tbtn_resume = self.toolbar.AddTool(-1, '', icons.resume_32.GetBitmap(), shortHelp='Resume')
-
         self.toolbar.AddSeparator()
-        tbtn_exit = self.toolbar.AddTool(-1, '', icons.exit_32.GetBitmap())
+        tbtn_change_mac = self.toolbar.AddTool(-1, '', icons.mac_32.GetBitmap(), shortHelp='Change MAC Address')
+        tbtn_alias = self.toolbar.AddTool(-1, '', icons.alias_32.GetBitmap(), shortHelp='Give an alias')
+        self.toolbar.AddSeparator()
+        tbtn_exit = self.toolbar.AddTool(-1, '', icons.exit_32.GetBitmap(), shortHelp='Exit')
 
         self.Bind(wx.EVT_TOOL, self.on_refresh, tbtn_refresh)
         self.Bind(wx.EVT_TOOL, self.on_cut, tbtn_cut)
         self.Bind(wx.EVT_TOOL, self.on_resume, tbtn_resume)
+        self.Bind(wx.EVT_TOOL, self.on_change_mac, tbtn_change_mac)
+        self.Bind(wx.EVT_TOOL, self.on_give_alias, tbtn_alias)
         self.Bind(wx.EVT_TOOL, self.on_exit, tbtn_exit)
 
     def set_status(self, msg):
@@ -109,10 +115,34 @@ class MainFrameView(MainFrame):
 
     def on_exit(self, event):
         self.unprotect()
+        self.aliases.close()
         self.Close()
 
     def on_refresh(self, event):
         self.trigger_thread()
+
+    def on_change_mac(self, event):
+        res = requests.get('http://127.0.0.1:8013/change-mac/{}'.format(self._gw['iface']))
+        if res.status_code == 200:
+            if res.json()['result']['status'] == 'success':
+                print('MAC Address changed')
+            elif  res.json()['result']['status'] == 'failed':
+                print('Couldn\'t change MAC')
+
+    def on_give_alias(self, event):
+        row = self.hosts_view.GetSelectedRow()
+        if row == wx.NOT_FOUND:
+            self.show_dialog('error', 'No Computer selected', 'Please select a computer from the list')
+        else:
+            mac = self.hosts_view.GetTextValue(row, 2)
+            dialog = wx.TextEntryDialog(None,
+                        'Enter an alias for the computer with MAC "{}" !'.format(mac),
+                        'Text Entry', 'My Computer', style=wx.OK|wx.CANCEL)
+            if dialog.ShowModal() == wx.ID_OK:
+                alias = dialog.GetValue()
+                self.aliases[mac] = alias
+                self.trigger_thread()
+            dialog.Destroy()
 
     def t_get_hosts(self):
         res = requests.get('http://127.0.0.1:8013/scan/{}'.format(self._my['ip']))
@@ -146,11 +176,16 @@ class MainFrameView(MainFrame):
 
             else:
                 host_icon = wx.dataview.DataViewIconText('', icon=icons.offline_24.GetIcon())
+            try:
+                alias = self.aliases[host['mac']]
+            except KeyError:
+                alias = ''
             self.hosts_view.AppendItem([
                 host_icon,
                 host['ip'],
                 host['mac'],
                 host['hostname'],
+                alias
             ])
 
         self.PushStatusText('Ready')
