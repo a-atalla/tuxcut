d6285146a98a1a66e94c19c7035e4d2ad3e0587b
a.atalla <a.3talla@gmail.com>
2017-10-21 08:06:52 +0200

    add download count

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $.ajax({
+    url: 'https://api.github.com/repos/a-atalla/tuxcut/releases',
+    method: 'GET',
+    success: function (data) {
+      var totalCount = data[0].assets[0].download_count + data[0].assets[1].download_count
+      $('#count').text(totalCount)
+    }
+  })
+})
